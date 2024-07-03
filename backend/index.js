791d39b95f074c413f5a64116c73ed713698163b
Gaurav Chauhan <115698661+Gauravchauhan48@users.noreply.github.com>
2024-07-03 09:38:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+	return res.json({
+		success: true,
+		message: "Your server is up and running ...",
+	});
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
